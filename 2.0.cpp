8c6038245ab92ab817d482c6afbf08ee95c5658f
xiaoqian <xiaoqian@ubuntu.ubuntu-domain>
2013-06-14 14:36:01 +0800

    2.0 commit

---
+++ 2.0.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class A{
+	private:
+		int value;
+	public:
+		A(int n) {
+			value = n;
+		}
+		A(const A& other) {
+			value = other.value;
+		}
+		void print() {
+			cout << value << endl;
+		}
+};
+
+int main()
+{
+	A a(10);
+	A b = a;
+	a.print();
+	b.print();
+	system("pause");
+	return 0;
+}
+
